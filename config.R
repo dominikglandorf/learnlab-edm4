364f9ac513b9fc77751fb8aa0a0b0a2718bfdabd
Dominik Glandorf <do.gl@gmx.de>
2022-07-20 20:07:11 +0200

    analyzed duo

--- config.R
+++ config.R
@@ -1,1 +1,5 @@
-path_french_language_learning = "ds74_student_step_All_Data_65_2021_0810_051112.txt"
+path_french_language_learning = "~/Documents/Studium/AIED/Summerschool/ds74_tx_2022_0716_034121/ds74_tx_All_Data_65_2021_0810_050335.txt"
+
+path_anki = "/Users/dglandorf/Documents/Studium/Psychologie/_LA/Glandorf_2021/data_dominik.csv"
+
+path_duo = "/Users/dglandorf/Documents/Studium/AIED/Summerschool/learning_traces.13m.csv"
